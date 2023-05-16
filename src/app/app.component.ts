85fdb348542efcdfe60e43bf6de6e67d81eb0ce9
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-16 13:41:58 +0700

    show list articles by fetch API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { NewsService } from './services/news.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Angular.TailwindCSS';
+  sources: any = [];
+  articles: any = [];
+  selectedNewsChannel: string = "Top 10 Trending News!";
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver, private cdr: ChangeDetectorRef, private newsapi: NewsService) {
+
+  }
+
+  ngOnInit(): void {
+    this.newsapi.initArticles().subscribe((res:any) => {
+      console.log(res);
+      this.articles = res.articles;
+    })
+    this.newsapi.initSources().subscribe((res:any) => {
+      console.log(res);
+      this.sources = res.sources;
+    })
+  }
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:800px)'])
+      .subscribe((res) => {
+        if (res?.matches) {
+          this.sideNav.mode = "over";
+          this.sideNav.close();
+        } else {
+          this.sideNav.mode = "side";
+          this.sideNav.open();
+        }
+      });
+    this.cdr.detectChanges();
+  }
+
+  searchSource(source: any) {
+    this.newsapi.getArticlesByID(source.id)
+      .subscribe((res: any) => {
+        this.selectedNewsChannel = source.name
+        this.articles = res.articles
+      })
+  }
 }
---
+++ src/app/services/news.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ServicesService } from './news.service';
+
+describe('ServicesService', () => {
+  let service: ServicesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ServicesService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
